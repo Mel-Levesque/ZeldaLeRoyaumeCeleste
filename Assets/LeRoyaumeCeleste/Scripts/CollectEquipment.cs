5297f3b849101c88a68c180b1e3c808636260161
melim <lvsqmelanie@gmail.com>
2023-11-14 12:23:53 +0100

    add equipment script

---
+++ Assets/LeRoyaumeCeleste/Scripts/CollectEquipment.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectEquipment : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<Renderer>().enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "PlayerCapsule")
+        {
+            gameObject.GetComponent<Renderer>().enabled = false;
+            var particle_equipment = GameObject.Find("Particle System_equiment");
+            particle_equipment.GetComponent<Renderer>().enabled = false;
+        }
+    }
+}
