632038afd9ab1d90d7c4b258c316a8c131ad0386
Killianlv <62741839+killianlv@users.noreply.github.com>
2023-11-14 11:05:27 +0100

    add next level

---
+++ Assets/Scripts/MoverLevel.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoverLevel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider collider) {
+        if (collider.tag == "LevelExit") {
+            Debug.Log("Level terminé");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
