79da1eb07608d7259e2381a3e2ca3aeea18f1c9c
Killianlv <62741839+killianlv@users.noreply.github.com>
2023-11-29 14:53:40 +0100

    add game over

--- Assets/LeRoyaumeCeleste/Scripts/Health.cs
+++ Assets/LeRoyaumeCeleste/Scripts/Health.cs
@@ -12,15 +12,19 @@ public class Health : MonoBehaviour
     Sprite fullHeart;
     Sprite emptyHeart;
 
+    private bool showGameOver = false;
+    private float gameOverDuration = 5.0f;
+    private string gameOverMessage = "Game Over";
+
     void Update()
     {
-        if(health > numOfHearts)
+        if (health > numOfHearts)
         {
             health = numOfHearts;
         }
         for (int i = 0; i < hearts.Length; i++)
         {
-            if(i < numOfHearts)
+            if (i < numOfHearts)
             {
                 hearts[i].enabled = true;
             }
@@ -32,8 +36,11 @@ public class Health : MonoBehaviour
 
         if (health <= 0)
         {
-            Debug.LogError("Fermer le jeux");
-            // Vérifier si nous sommes en dehors de l'éditeur Unity
+            // Afficher le message "Game Over" en plein écran
+            showGameOver = true;
+            StartCoroutine(HideGameOverAfterSeconds(gameOverDuration));
+
+            // Fermer le jeu
             #if UNITY_EDITOR
             UnityEditor.EditorApplication.isPlaying = false;
             #else
@@ -49,4 +56,55 @@ public class Health : MonoBehaviour
         Debug.LogError("health -1");
     }
 
+    private IEnumerator HideGameOverAfterSeconds(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        showGameOver = false;
+    }
+
+    private void OnGUI()
+    {
+        if (showGameOver)
+        {
+            // Mesurer la taille du texte
+            GUIStyle style = new GUIStyle();
+            style.alignment = TextAnchor.MiddleCenter;
+            style.fontSize = 40;
+            GUIContent content = new GUIContent(gameOverMessage);
+            Vector2 textSize = style.CalcSize(content);
+
+            // Calculer les dimensions du rectangle en fonction de la taille du texte
+            float padding = 10f; // Marge autour du texte
+            float rectWidth = textSize.x + 2 * padding;
+            float rectHeight = textSize.y + 2 * padding;
+            float rectX = (Screen.width - rectWidth) / 2;
+            float rectY = (Screen.height - rectHeight) / 2;
+
+            // Afficher un rectangle de couleur foncée en arrière-plan
+            GUIStyle darkRectStyle = new GUIStyle();
+            darkRectStyle.normal.background = MakeTexture((int)Screen.width, (int)Screen.height, Color.black);
+            GUI.Box(new Rect(0, 0, Screen.width, Screen.height), "", darkRectStyle);
+
+            // Afficher le rectangle avec le message "Game Over"
+            GUIStyle redRectStyle = new GUIStyle();
+            redRectStyle.normal.background = MakeTexture((int)rectWidth, (int)rectHeight, Color.red);
+            GUI.Box(new Rect(rectX, rectY, rectWidth, rectHeight), "", redRectStyle);
+
+            // Afficher le message au milieu du rectangle
+            GUI.Label(new Rect(rectX + padding, rectY + padding, textSize.x, textSize.y), gameOverMessage, style);
+        }
+    }
+
+    private Texture2D MakeTexture(int width, int height, Color color)
+    {
+        Color[] pixels = new Color[width * height];
+        for (int i = 0; i < pixels.Length; i++)
+        {
+            pixels[i] = color;
+        }
+        Texture2D texture = new Texture2D(width, height);
+        texture.SetPixels(pixels);
+        texture.Apply();
+        return texture;
+    }
 }
